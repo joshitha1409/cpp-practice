c00f54b946e29bef8ea82879e54c9ed1e0f6b4ff
Joshitha M <joshitha1409@gmail.com>
2026-07-23 12:12:12 +0530

    Add prime number checker program

---
+++ Number Theory/PrimeNumber.cpp
@@ -0,0 +1,33 @@
+// Program to check whether a number is prime
+// Language: C++
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    bool isPrime=true;
+    cout<<"enter the number: ";
+    cin>>num;
+
+    if(num<=1){
+            cout<<"not prime"; 
+            return 0;
+        }
+    for(int i=2;i<=num-1;i++){
+        if(num%i==0){
+            isPrime=false;
+            break;
+        }
+        else{
+            isPrime=true;
+        }
+    }
+    if(isPrime==true){
+        cout<<"Prime";
+    }
+    else{
+        cout<<"Not prime";
+    }
+    return 0;
+}
