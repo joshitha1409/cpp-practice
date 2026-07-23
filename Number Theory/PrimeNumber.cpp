15195127fff64b68e6dc416832c03752436b1f0b
Joshitha M <joshitha1409@gmail.com>
2026-07-23 19:08:16 +0530

    PrimeNumber.cpp

--- Number Theory/PrimeNumber.cpp
+++ Number Theory/PrimeNumber.cpp
@@ -1,6 +1,8 @@
 // Program to check whether a number is prime
 // Language: C++
 
+// for(int i=2;i*i<=num;i++) -> we can use this loop as well since we check from 2 to underroot n.. so instead of underroot we square on both sides :o
+
 #include<iostream>
 using namespace std;
 
